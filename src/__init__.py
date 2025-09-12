b8b84a73b6ce2a71f260aebdc0bcfd0f1536955b
Pedro Rogel <pedrorogel3@gmail.com>
2025-09-12 18:47:41 -0300

    feat: add cors to allow access to fontend

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from src.config_db.db import Config_db
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
@@ -9,6 +10,7 @@ def create_app(config_class=Config_db,):
     app = Flask(__name__)
     app.config.from_object(config_class)
     db.init_app(app)
+    CORS(app)
 
     from src.route.produtos.route import init_routeClients
     from src.route.clientes.route import init_routeProduct
