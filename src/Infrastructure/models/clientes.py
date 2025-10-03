1381d44690ff3bdd2b94a02ad87c3101a767ecea
Pedro Rogel <pedrorogel3@gmail.com>
2025-10-03 16:40:41 -0300

    feat: add new fields to client entity

--- src/Infrastructure/models/clientes.py
+++ src/Infrastructure/models/clientes.py
@@ -1,5 +1,5 @@
 from src import db
-from sqlalchemy import Integer, Column, String, Date
+from sqlalchemy import Integer, Column, String, Date, Text
 
 class Cliente(db.Model):
     __tablename__ = "clientes"
@@ -8,6 +8,10 @@ class Cliente(db.Model):
     nome = (Column(String(255), nullable=False))
     cpf = (Column(String(255), nullable=False))
     data_nascimento = (Column(Date, nullable=False))
+    turno = (Column(Text, nullable=False))
+    sala = Column(Text, nullable=False)
+    numero = Column(String(25), nullable=False)
+    email = Column(Text, nullable=False)
     
     @property
     def data_nascimento_formatada(self):
@@ -18,5 +22,9 @@ class Cliente(db.Model):
             "id": self.id,
             "nome": self.nome,
             "cpf": self.cpf,
-            "data_nascimento": self.data_nascimento.isoformat()
+            "data_nascimento": self.data_nascimento.isoformat(),
+            "numero" : self.numero,
+            "sala": self.sala,
+            "turno": self.turno,
+            "email": self.email
         }
