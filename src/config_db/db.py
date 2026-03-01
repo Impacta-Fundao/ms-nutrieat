c75fbf5188cc7864b78e1ae6afaa347b4dfad423
Pedro Rogel <pedrorogel3@gmail.com>
2026-03-01 17:31:49 -0300

    feat: add new endpoint sales quantity by month

--- src/config_db/db.py
+++ src/config_db/db.py
@@ -11,3 +11,4 @@ class Config_db:
     DB_PORT = os.getenv("DB_PORT")
     DB_NAME = os.getenv("DB_NAME")
     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    
